555c082d752d4c44248756d154729a311a8f43ab
Zachary Casares <rrumble250@gmail.com>
2023-02-21 13:57:03 -0500

    add validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "testin testin read all about it"
-  
+  user = User.first_or_create(email: "example@test.com", password: "password")
+
+  it 'has a password' do 
+    expect(user).to be_valid
+  end
+
+  context 'without password' do
+    it 'should be invalid' do
+      user.password = nil
+      expect(user).to be_valid
+    end
+  end
+
+  context 'without email' do
+    it 'should be invalid' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+  end
+
 end
