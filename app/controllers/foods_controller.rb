e5cc20f09de48096b60171c495a57c143a94a657
Zachary Casares <rrumble250@gmail.com>
2023-02-21 17:24:49 -0500

    add foods controller and basic actions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,29 @@
 class FoodsController < ApplicationController
 
+  def index
+    @foods = Food.all
+  end
 
-  
+  def new
+    @food = Food.new
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def create
+    if @food.save
+      redirect_to root_path
+    end
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
 end
