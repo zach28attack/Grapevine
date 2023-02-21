c39b01a979e98f3a77e446f5be6fc4de02b793fd
Zachary Casares <rrumble250@gmail.com>
2023-02-21 14:47:46 -0500

    create foods table

--- db/migrate/20230221193741_foods.rb
+++ db/migrate/20230221193741_foods.rb
@@ -1,4 +1,14 @@
 class Foods < ActiveRecord::Migration[7.0]
   def change
+    create_table :foods do |f|
+      f.string :food_name
+      f.integer :calories
+      f.integer :protein
+      f.integer :carbs
+      f.integer :fats
+      f.integer :servings
+
+      f.timestamps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_192634) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_193741) do
+  create_table "foods", force: :cascade do |t|
+    t.string "food_name"
+    t.integer "calories"
+    t.integer "protein"
+    t.integer "carbs"
+    t.integer "fats"
+    t.integer "servings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
