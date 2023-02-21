fe43a9db3a0dfc933e69b0863f98a64048d03d69
Zachary Casares <rrumble250@gmail.com>
2023-02-21 18:59:22 -0500

    add validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,10 @@
 class Food < ApplicationRecord
 
+  validates :food_name, presence: true, length:{minimum:2, maximum:20}
+  validates :calories, presence: true, length:{minimum:1, maximum:20}
+  validates :protein, presence: true, length:{minimum:1, maximum:20}
+  validates :fats, presence: true, length:{minimum:1, maximum:20}
+  validates :carbs, presence: true, length:{minimum:1, maximum:20}
+  validates :servings, presence: true, length:{minimum:1, maximum:20}
+
 end
