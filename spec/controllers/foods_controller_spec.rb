ac3e08c6f286a2b6504695b9e153179bcb520a34
Zachary Casares <rrumble250@gmail.com>
2023-02-22 14:51:48 -0500

    add food#create tests

--- spec/controllers/foods_controller_spec.rb
+++ spec/controllers/foods_controller_spec.rb
@@ -3,35 +3,65 @@ require "rails_helper"
 RSpec.describe FoodsController, :type => :controller do
   let!(:food)  {Food.create(food_name: "eggs", calories: 70, protein: 23, fats: 10, carbs: 0, servings: 1)}
 
-  describe "GET index" do
+  describe "GET #index" do
     it "has a 200 status code" do
       get :index
       expect(response.status).to eq(200)
     end
   end
 
-  describe "GET new" do
+  describe "GET #new" do
     it "has 200 status code" do
       get :new
       expect(response.status).to eq(200)
     end
   end
 
-  describe "GET show" do
+  describe "POST #create" do
+
+    context "with valid attributes" do
+      it "creates new food" do
+        expect {
+          post :create, params: { food: { food_name: "salmon", calories: 500, protein: 50, fats: 20, carbs: 0, servings: 1 }}
+      }.to change(Food, :count).by(1)
+      end
+
+      it "redirects to root" do
+        post :create, params: { food: { food_name: "salmon", calories: 500, protein: 50, fats: 20, carbs: 0, servings: 1 }}
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "creates new food" do
+        expect {
+          post :create, params: { food: { food_name: "", calories: nil, protein: nil, fats: nil, carbs: nil, servings: nil }}
+      }.to_not change(Food, :count)
+      end
+
+      it "redirects to root" do
+        post :create, params: { food: { food_name: "", calories: nil, protein: nil, fats: nil, carbs: nil, servings: nil }}
+        expect(response).to render_template(:new)
+      end
+    end
+
+  end
+
+  describe "GET #show" do
     it "returns successful" do
       get :show, params: {id: food.id}
       expect(response).to be_successful # same as expect(response.status).to eq(200)
     end
   end
 
-  describe "GET edit" do
+  describe "GET #edit" do
     it "returns successful" do
       get :edit, params: {id: food.id}
       expect(response).to be_successful 
     end
   end
 
-  describe "PATCH update" do
+  describe "PATCH #update" do
     context "with valid attributes" do
       it "updates food and redirects to show page" do
         patch :update, params: {id: food.id, food: {food_name: "Updated food name"}}
@@ -51,7 +81,7 @@ RSpec.describe FoodsController, :type => :controller do
     end
   end
 
-  describe "DELETE food" do
+  describe "DELETE #destroy" do
     it "deletes food and redirects to index" do
       expect { delete :destroy, params: {id: food.id}}.to change(Food, :count).by(-1)
       expect(response).to redirect_to(foods_path)
