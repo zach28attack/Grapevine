0cdadf6c5fb5dc9e4d6d8507e3a33543b6edc310
Zachary Casares <rrumble250@gmail.com>
2023-02-23 15:16:28 -0500

    make diaries index root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :foods
   resources :diaries
-  root "home#index"
+  root "diaries#index"
   get "welcome", to:"welcome#welcome"
 end
