31607a1f9d376cfdba3a569c5a74ab183ea99786
Zachary Casares <rrumble250@gmail.com>
2023-02-27 17:12:18 -0500

    add actions to controller

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,3 +1,19 @@
-class Meals < ApplicationController
+class MealsController < ApplicationController
 
+  def index
+    @meals = Meal.all
+  end
+
+  def create
+    m = Meal.new(meal_params)
+    if m.save
+      redirect_to meals_path
+    end
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:meal_name)
+  end
 end
