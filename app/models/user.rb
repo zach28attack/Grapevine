ef18c71a788adc26195fb57cb341aee016c89a62
Zachary Casares <rrumble250@gmail.com>
2023-02-27 15:41:07 -0500

    add association to foods_meals

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :diaries, class_name: "Diary", dependent: :destroy
-
+  has_many :foods_meal, dependent: :destroy
 end
