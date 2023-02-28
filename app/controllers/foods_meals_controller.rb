91848bbb90583c9355a7c6f8367f5090172441b0
Zachary Casares <rrumble250@gmail.com>
2023-02-28 14:16:42 -0500

    add new and create to controller

--- app/controllers/foods_meals_controller.rb
+++ app/controllers/foods_meals_controller.rb
@@ -1,3 +1,21 @@
-class Meals < ApplicationController
+class FoodsMealsController < ApplicationController
+
+def new
+  @meal = Meal.find(params[:id])
+  @foodsMeal = FoodsMeal.new
+end
+
+def create
+  @foodsMeal = FoodsMeal.new(foods_meal_params)
+  @foodsMeal.user_id = current_user.id
+  if @foodsMeal.save
+    redirect_to diaries_path
+  end
+end
+
+private
+  def foods_meal_params
+    params.require(:foods_meal).permit(:food_id, :meal_id)
+  end
 
 end
