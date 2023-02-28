b26d309e3bef97014b49c586dab21ae6d94a573c
Zachary Casares <rrumble250@gmail.com>
2023-02-28 18:01:23 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
     }
-  resources :foods
+  resources :foods, except: %i[ show ]
   resources :diaries
-  resources :meals
-  resources :foods_meals
+  resources :meals, only: %i[ index create destroy ]
+  resources :foods_meals, only: %i[ new create destroy]
   root "diaries#index"
   get "welcome", to:"welcome#welcome"
 end
