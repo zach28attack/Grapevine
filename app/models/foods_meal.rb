607beb3fef0ed1d9c533bde189f9a193f3564855
Zachary Casares <rrumble250@gmail.com>
2023-02-28 23:01:52 -0500

    fix bug, delete food/meal on foods_meal deletion

--- app/models/foods_meal.rb
+++ app/models/foods_meal.rb
@@ -1,5 +1,5 @@
 class FoodsMeal < ApplicationRecord
-  belongs_to :food,  dependent: :destroy
-  belongs_to :meal,  dependent: :destroy
+  belongs_to :food
+  belongs_to :meal
   belongs_to :user
 end
