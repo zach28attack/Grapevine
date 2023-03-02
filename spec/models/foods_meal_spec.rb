ba9a2862d956f2aaa69d5a08ddf762ba1df87849
Zachary Casares <rrumble250@gmail.com>
2023-03-02 15:37:59 -0500

    add foods_meal unit test

--- spec/models/foods_meal_spec.rb
+++ spec/models/foods_meal_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe FoodsMeal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:meal) { FactoryBot.create(:meal) }
+  let!(:food) { FactoryBot.create(:food) }
+  let!(:foods_meal) { FactoryBot.create(:foods_meal, user: user) }
+  
+  context "with nil :meal_id" do
+    it "should be invalid" do
+      foods_meal.meal_id = nil
+      expect(foods_meal).to_not be_valid
+    end
+  end
+
+  context "with nil :food_id" do
+    it "should be invalid" do
+      foods_meal.food_id = nil
+      expect(foods_meal).to_not be_valid
+    end
+  end
 end
