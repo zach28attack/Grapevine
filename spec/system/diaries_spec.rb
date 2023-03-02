6cd58419b6aacc60179cbe9f41184f38c8aa1490
Zachary Casares <rrumble250@gmail.com>
2023-03-01 22:07:19 -0500

    add diary creation with foods_meal

--- spec/system/diaries_spec.rb
+++ spec/system/diaries_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Diaries", type: :system do
-  before do
-    driven_by(:rack_test)
-    sign_in user
-  end
   let!(:user) { FactoryBot.create(:user) }
   let!(:breakfast_diary) { FactoryBot.create(:diary, time_of_day: "breakfast", user: user) }
   let!(:lunch_diary) { FactoryBot.create(:diary, time_of_day: "lunch", user: user) }
   let!(:dinner_diary) { FactoryBot.create(:diary, time_of_day: "dinner", user: user) }
-  let(:food) {FactoryBot.create(:food)}
+  let(:food) { FactoryBot.create(:food) }
+  let(:meal) { FactoryBot.create(:meal) }
+  let(:foods_meal) { FactoryBot.create(:foods_meal, user: user, food_id: food.id, meal_id: meal.id) }
+  
+  before do
+    driven_by(:rack_test)
+    sign_in user
+  end
 
   describe "GET #index" do
     it "should have 200 status code" do 
-      
       visit diaries_path
       expect(page.status_code).to eq(200)
     end
@@ -51,13 +53,13 @@ RSpec.describe "Diaries", type: :system do
           fill_in "diary[fats_eaten]", with: 20
           fill_in "diary[carbs_eaten]", with: 15
           click_button "Create Diary"
-        }.to change(Diary, :count).by(0)
+        }.to_not change(Diary, :count)
         expect(page.status_code).to eq(422)
       end
     end
 
-    context "with already added foods" do
-      it "should populate diary form with a foods values" do
+    context "using pre-populated foods" do
+      it "should save diary" do
         expect{
           visit new_diary_path
           click_button "Add food"
@@ -65,5 +67,17 @@ RSpec.describe "Diaries", type: :system do
         expect(page).to have_current_path(diaries_path)
       end
     end
+
+    context "with pre-populated foods_meals" do
+      it "should save diary" do
+        expect {
+          foods_meal
+          visit new_diary_path
+          save_and_open_page
+          click_button "Add meal"
+        }.to change(Diary, :count).by(1)
+      end
+    end
+
   end
 end
