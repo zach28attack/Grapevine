d1ad922dab44a94754cb36f5e34ab6b76865b6f6
Zachary Casares <rrumble250@gmail.com>
2023-03-02 15:30:25 -0500

    add numericality validation

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,8 +1,8 @@
 class Diary < ApplicationRecord
   belongs_to :user, class_name: "User"
-  validates :calories_eaten, presence: true
-  validates :protein_eaten, presence: true
-  validates :fats_eaten, presence: true
-  validates :carbs_eaten, presence: true
+  validates :calories_eaten, presence: true, numericality: { only_integer: true }, length: {minimum:1, maximum:4}
+  validates :protein_eaten, presence: true, numericality: { only_integer: true }, length: {minimum:1, maximum:4}
+  validates :fats_eaten, presence: true, numericality: { only_integer: true }, length: {minimum:1, maximum:4}
+  validates :carbs_eaten, presence: true, numericality: { only_integer: true }, length: {minimum:1, maximum:4}
   belongs_to :meal, optional: true
 end
