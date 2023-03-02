be1a840cf762b0f5b2af03e0b15bf19229529d45
Zachary Casares <rrumble250@gmail.com>
2023-03-02 13:54:29 -0500

    add tests for controller

--- spec/system/foods_meals_spec.rb
+++ spec/system/foods_meals_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "FoodsMeals", type: :system do
+  let!(:user) { FactoryBot.create(:user) }
+  let(:food) { FactoryBot.create(:food) }
+  let(:meal) { FactoryBot.create(:meal) }
+
   before do
     driven_by(:rack_test)
+    sign_in user
   end
 
-  
+  describe "POST #create" do
+    it "creates new foods_meal" do
+      expect {
+        meal
+        visit new_foods_meal_path(id: meal.id)
+        click_button "Add food"
+      }.to change(FoodsMeal, :count).by(1)
+    end
+  end
 end
