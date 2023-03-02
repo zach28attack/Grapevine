1230726f0fadc878c81a7e929c6113acaebeb090
Zachary Casares <rrumble250@gmail.com>
2023-03-02 15:31:04 -0500

    reduce max digits from 5 to 4 for realistic input

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,10 +1,10 @@
 class Food < ApplicationRecord
   validates :food_name, presence: true, length:{minimum:2, maximum:20}
-  validates :calories, presence: true, length:{minimum:1, maximum:5}, numericality: { only_integer: true }
-  validates :protein, presence: true, length:{minimum:1, maximum:5}, numericality: { only_integer: true }
-  validates :fats, presence: true, length:{minimum:1, maximum:5}, numericality: { only_integer: true }
-  validates :carbs, presence: true, length:{minimum:1, maximum:5}, numericality: { only_integer: true }
-  validates :servings, presence: true, length:{minimum:1, maximum:5}, numericality: { only_integer: true }
+  validates :calories, presence: true, length:{minimum:1, maximum:4}, numericality: { only_integer: true }
+  validates :protein, presence: true, length:{minimum:1, maximum:4}, numericality: { only_integer: true }
+  validates :fats, presence: true, length:{minimum:1, maximum:4}, numericality: { only_integer: true }
+  validates :carbs, presence: true, length:{minimum:1, maximum:4}, numericality: { only_integer: true }
+  validates :servings, presence: true, length:{minimum:1, maximum:4}, numericality: { only_integer: true }
   
   has_many :foods_meals
   has_many :meals, through: :foods_meals
