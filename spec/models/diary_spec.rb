c67d9d05f8bdd1a269c6e137ffd372c3d2f97905
Zachary Casares <rrumble250@gmail.com>
2023-03-02 15:30:04 -0500

    add diary unit tests

--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:diary) { FactoryBot.create(:diary, user: user) }
+
+  context "when creating a valid Diary" do
+    it "should be valid" do
+      expect(diary).to be_valid
+    end
+  end
+
+  context "when creating an invalid Diary" do
+
+    it "should be invalid without :user" do
+      diary.user_id = nil
+      expect(diary).to_not be_valid
+    end
+
+    it "should be invalid without :calories_eaten" do
+      diary.calories_eaten = nil
+      expect(diary).to_not be_valid
+    end
+    it "should be invalid with value longer than 5 digits" do
+      diary.calories_eaten = 10000
+      expect(diary).to_not be_valid
+    end
+
+    it "should be invalid without :protein_eaten" do
+      diary.protein_eaten = nil
+      expect(diary).to_not be_valid
+    end
+    it "should be invalid with value longer than 5 digits" do
+      diary.protein_eaten = 10000
+      expect(diary).to_not be_valid
+    end
+    
+    it "should be invalid without :fats_eaten" do
+      diary.fats_eaten = nil
+      expect(diary).to_not be_valid
+    end
+    it "should be invalid with value longer than 5 digits" do
+      diary.fats_eaten = 10000
+      expect(diary).to_not be_valid
+    end
+
+    it "should be invalid without :carbs_eaten" do
+      diary.carbs_eaten = nil
+      expect(diary).to_not be_valid
+    end
+    it "should be invalid with value longer than 5 digits" do
+      diary.carbs_eaten = 10000
+      expect(diary).to_not be_valid
+    end
+  end
 end
