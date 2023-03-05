0b583f5587e15dea9db526da936b9a0ff793342a
Zachary Casares <rrumble250@gmail.com>
2023-03-04 19:31:37 -0500

    add validation for meal_name

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,6 @@
 class Meal < ApplicationRecord
+  validates :meal_name, presence: true, length:{minimum:2, maximum: 10}
+
   validates :meal_name, presence: true
   has_many :foods_meals
   has_many :foods, through: :foods_meals
