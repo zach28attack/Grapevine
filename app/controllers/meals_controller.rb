4e61e876e0d871bba7a6fcdd25642663c85da027
Zachary Casares <rrumble250@gmail.com>
2023-03-09 16:47:20 -0500

    allow time_of_day param and set params manually

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -5,15 +5,16 @@ class MealsController < ApplicationController
   end
 
   def create
-    m = Meal.new(meal_params)
+    m = Meal.new
+    m.meal_name = meal_params[:meal_name]
     if m.save
-      redirect_to new_foods_meal_path(id: m.id)
+      redirect_to new_foods_meal_path(id: m.id, time_of_day: meal_params[:time_of_day])
     end
   end
 
   private
 
   def meal_params
-    params.require(:meal).permit(:meal_name)
+    params.require(:meal).permit(:meal_name, :time_of_day) #get the time of day as a param
   end
 end
