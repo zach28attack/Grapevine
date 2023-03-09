fc1daa16f2c28e4ac09e3f42df815fd8bc638faf
Zachary Casares <rrumble250@gmail.com>
2023-03-09 16:47:31 -0500

    allow time_of_day param and set params manually

--- app/controllers/foods_meals_controller.rb
+++ app/controllers/foods_meals_controller.rb
@@ -3,26 +3,29 @@ class FoodsMealsController < ApplicationController
 def new
   @meal = Meal.find(params[:id])
   @foodsMeal = FoodsMeal.new
+  @time_of_day = params[:time_of_day] #set the @time_of_day variable with the time_of_day parameter 
 end
 
 def create
-  foodsMeal = FoodsMeal.new(foods_meal_params)
-  foodsMeal.user_id = current_user.id
-  if foodsMeal.save
-    redirect_to new_foods_meal_path(id: foodsMeal.meal_id)
+  foods_meal = FoodsMeal.new
+  foods_meal.food_id = foods_meal_params[:food_id]
+  foods_meal.meal_id = foods_meal_params[:meal_id]
+  foods_meal.user_id = current_user.id
+  if foods_meal.save
+    redirect_to new_foods_meal_path(id: foods_meal.meal_id, time_of_day: foods_meal_params[:time_of_day])
   end
 end
 
 def destroy
-  foodsMeal = FoodsMeal.find(params[:id])
-  if foodsMeal.destroy
+  foods_meal = FoodsMeal.find(params[:id])
+  if foods_meal.destroy
     redirect_to new_foods_meal_path
   end
 end
 
 private
   def foods_meal_params
-    params.require(:foods_meal).permit(:food_id, :meal_id)
+    params.require(:foods_meal).permit(:food_id, :meal_id, :time_of_day) #get the time of day as a param
   end
 
 end
